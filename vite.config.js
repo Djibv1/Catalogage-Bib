a0d82fd8fbb4e7df12dc6d3faa88a6d07a0559d3
Djibv1 <djilanibourhan@yahoo.fr>
2025-10-13 22:42:00 +0200

    deploy build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Catalogage-Bib/",
   plugins: [react()],
+  base: "/Catalogage-Bib/", // ← ajoute cette ligne !
 });
